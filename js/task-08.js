9d5a2f6d2f0e333aef5a1273227ed6457e5085f8
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-04-18 07:29:45 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,26 +1,43 @@
-const loginForm = document.querySelector('.login-form');
+// const loginForm = document.querySelector('.login-form');
 
-loginForm.addEventListener('submit', (event) => {
+// loginForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const formValues = {};
+
+//   for (let i = 0; i < loginForm.elements.length; i++) {
+//     const element = loginForm.elements[i];
+//     if (element.type !== 'submit') {
+//       if (!element.value) {
+//         alert('All fields are required');
+//         return;
+//       }
+//       formValues[element.name] = element.value;
+//     }
+//   }
+
+//   console.log(formValues);
+
+//   loginForm.reset();
+// });
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formValues = {};
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
 
-  for (let i = 0; i < loginForm.elements.length; i++) {
-    const element = loginForm.elements[i];
-    if (element.type !== 'submit') {
-      if (!element.value) {
-        alert('All fields are required');
-        return;
-      }
-      formValues[element.name] = element.value;
-    }
+  if (!email || !password) {
+    alert("All fields are required");
+    return;
   }
 
+  const formValues = { email, password };
   console.log(formValues);
 
   loginForm.reset();
 });
-
 
 
 
